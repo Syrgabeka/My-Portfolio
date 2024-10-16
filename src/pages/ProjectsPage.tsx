967b81727e6f73d3ae4b28b0cdd0b6ca1c5590e7
Syrgabek <syrgabeka766@gmail.com>
2024-10-16 18:31:30 +0600

    ..

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -10,7 +10,7 @@ const projects: projectsTypes[] = [
     description:
       "HealthCheck is a project designed to facilitate interaction between patients and doctors in today's digital world. HealthCheck provides a convenient way to join an online queue for consultations and examinations, which saves time and simplifies access to medical care. Patients can receive their exam results in an online format, see current hospital workload, view brief profiles of doctors, and read reviews from other patients. This makes the process more transparent and accessible to each user.",
     stack: ["TypeScript", "ReduxToolkit", "RestAPI"],
-    codeLink: "",
+    codeLink: "https://github.com/Syrgabeka/Healtch-Check",
     demoLink: "",
     image: HealtChexk,
   },
